3e1559fdb8f85e8ccffdf57fe022a2d609da559c
JayVB4 <bhadrashete3@gmail.com>
2024-09-21 18:19:38 +0530

    frontend_initialized

---
+++ frontend/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// import Navbar from 'src/components/Navbar';
+import Navbar from "../components/Navbar"; // Use relative path
+
+
+export function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
